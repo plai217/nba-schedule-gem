865c7ed115eb392aecbce6f9f44820d46fdc4938
plai217 <philiplai217@gmail.com>
2015-12-07 20:26:16 -0500

    added schedules scores and game summary

--- lib/cli.rb
+++ lib/cli.rb
@@ -5,10 +5,8 @@ require 'open-uri'
 
 class Nbascraper::CLI
 
-  attr_reader :data, :schedule, :scores
-
   def start
-    today
+    Nbascraper.today
     list
   end
 
@@ -17,28 +15,22 @@ class Nbascraper::CLI
     until selection == "5"
       puts "Enter 1 to refresh today's scores"
       puts "Enter 2 to see schedules and scores for another date"
+      puts "Enter 3 to get a game preview or recap"
       puts "Enter 5 to exit"
       selection = gets.strip
       case selection
       when "1"
-        today
+        Nbascraper.today
       when "2"
         new_date
       when "3"
-        puts "Place Holder"
+        Nbascraper.get_summary
       when "5"
         puts "Good-bye"
       else
         puts "Invalid selection."
       end 
     end    
-  end
-
-  def today
-    puts "Today's games"
-    date = Time.now.strftime("%Y%m%d")
-    get_data(date)
-    show_schedule_scores
   end
 
   def new_date
@@ -49,50 +41,13 @@ class Nbascraper::CLI
     puts "Please enter day of month in DD format"
     day = gets.strip
     date = "#{year}#{month}#{day}"
-    get_data(date)
-    show_schedule_scores
+    Nbascraper.get_games(date)
   end
 
-  def get_data(date)
-    @data = Nbascraper.scrape_index(date)
-  end
+end
 
-  def get_schedule
-    @schedule = {}
-    @data.each do |game|
-      @schedule[game["id"]] = "#{game["visitor"]["nickname"]} at #{game["home"]["nickname"]} starting #{game["time"]}"    
-    end
-    @schedule
+class DateError < StandardError
+  def message 
+    "Invalid Date"
   end
-
-  def get_scores
-    @scores = {}
-    @data.each do |game|
-      @scores[game["id"]] = "#{game["visitor"]["nickname"]} - #{game["visitor"]["score"]} #{game["home"]["nickname"]} - #{game["home"]["score"]}"    
-    end 
-    @scores   
-  end
-
-  def game_score(id)
-    @data.each do |game| 
-      return "#{game["visitor"]["nickname"]} - #{game["visitor"]["score"]} #{game["home"]["nickname"]} - #{game["home"]["score"]}" if game["id"] == id
-    end
-  end
-
-  def game_start(id)
-    @data.each do |game| 
-      return "#{game["visitor"]["nickname"]} at #{game["home"]["nickname"]} game starts #{game["time"]}" if game["id"] == id
-    end
-  end
-
-  def show_schedule_scores
-    @data.each do |game|
-      if game["visitor"]["score"] == ""
-        puts game_start(game["id"])
-      else
-        puts game_score(game["id"])
-      end
-    end
-  end
-
 end
--- lib/nbascraper.rb
+++ lib/nbascraper.rb
@@ -5,12 +5,78 @@ require 'json'
 
 class Nbascraper
 
+  attr_reader :data, :schedule, :scores, :player, :game_urls
+
+  def self.today
+    puts "*****Today's games*****"
+    date = Time.now.strftime("%Y%m%d")
+    get_data(date)
+    show_schedule_scores
+    puts "***********************"
+  end
+
+  def self.get_games(date)
+    puts "*****Schedule and scores for #{date}*****"
+    get_data(date)
+    show_schedule_scores
+    puts "******************************************"
+  end
+
+  def self.get_summary
+    @game_urls.each_with_index {|game,i| puts "#{i+1}. #{game}"} 
+    game_number = 0
+    until game_number.between?(1,(@game_urls.size)) 
+      puts "Please enter 1 - #{@game_urls.size}"
+      game_number = gets.strip.to_i
+    end
+    scrape_summary(@game_urls[game_number-1])
+  end
+
+  def self.get_data(date)
+    @data = scrape_index(date)
+    @game_urls = []
+    @data.each {|game| @game_urls << game["game_url"]}
+  end
+
+  def self.show_schedule_scores
+    @data.each do |game|
+      if game["time"] > Time.now.strftime("%H%M") && game["date"] >= Time.now.strftime("%Y%m%d")
+        puts game_start(game["id"])
+      else
+        puts game_score(game["id"])
+      end
+    end
+  end
+
+  def self.game_start(id)
+    @data.each do |game| 
+      return "#{game["visitor"]["nickname"]} at #{game["home"]["nickname"]} game starts #{game["time"]}" if game["id"] == id
+    end
+  end
+
+  def self.game_score(id)
+    @data.each do |game| 
+      return "#{game["visitor"]["nickname"]} - #{game["visitor"]["score"]} #{game["home"]["nickname"]} - #{game["home"]["score"]}" if game["id"] == id
+    end
+  end
+
   def self.scrape_index(date)
     url = "http://data.nba.com/data/1h/json/cms/noseason/scoreboard/#{date}/games.json"
     doc = JSON.parse(Nokogiri::HTML(open(url)))  
     doc["sports_content"]["games"]["game"]
   end
 
-end
+  def self.scrape_summary(game_url)
+    url = "http://www.nba.com/games/#{game_url}/gameinfo.html?ls=iref:nba:scoreboard"
+    doc = Nokogiri::HTML(open(url))
+    puts "****************************************"
+    if doc.css("#nbaGIRecap2 p").text == ""
+      puts doc.css("#nbaGIPreview p").text
+    else
+      puts doc.css("#nbaGIRecap2 p").text
+    end
+    puts "****************************************"
+  end
 
+end
 
